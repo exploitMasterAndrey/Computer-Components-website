a6971e8f9643a923cd20a38992a34a4bc7bc0f0c
Andrey <mister.andrey-steam@yandex.ru>
2021-12-05 14:43:27 +0300

    Законченная курсовая

---
+++ menuScript.js
@@ -0,0 +1,8 @@
+function openMenu()
+{
+    document.getElementById("nav").classList.toggle('active');
+}
+function openContact()
+{
+    document.getElementById("contact").classList.toggle('active');
+}
